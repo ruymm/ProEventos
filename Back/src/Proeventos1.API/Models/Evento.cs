efedd150fe635f1592cac3ad41a014df120435e1
Ruy Marcos Martins <ruy@agecomnet.com.br>
2024-05-11 18:30:41 -0300

    aula1

---
+++ Back/src/Proeventos1.API/Models/Evento.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proeventos1.API.Models
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtaPessoas { get; set; }
+        public string Lote { get; set; }
+        public string ImagemURL { get; set; }
+
+    }
+}
